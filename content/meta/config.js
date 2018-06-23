56d6f0d083048ec0aea63b91cb5deffc27abd28b
WangRui <maxieewong@gmail.com>
2018-06-24 02:05:53 +0800

    update info

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,31 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "Maxiee 笔记", // <title>
+  shortSiteTitle: "Maxiee 笔记", // <title> ending for posts and pages
+  siteDescription: "Maxiee 笔记，记录技术心得与编程感悟",
+  siteUrl: "https://maxiee.github.io/",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Maxiee",
+  authorTwitterAccount: "",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Maxiee",
+  infoTitleNote: "记录点滴技术心得",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "Maxiee 笔记",
+  manifestShortName: "Maxiee 笔记", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "maxieewong@gmail.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/maxiee" },
+    { name: "weibo", url: "https://weibo.com/maxiee" }
   ]
 };
